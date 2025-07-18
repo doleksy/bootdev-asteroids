1b0175194965f44a41f3052ffd8c811a99547dab
Dave Oleksy <dave@doleksy.com>
2025-07-17 20:16:26 -0500

    CH2: L8

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,13 +11,32 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    game_clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(color=(0,0,0))
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+
+        time_passed_ms = game_clock.tick(60)
+        dt = time_passed_ms / 1000
 
 if __name__ == "__main__":
     main()
